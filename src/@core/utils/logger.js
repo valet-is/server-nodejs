8a18ba2743e6e107f57e30d791a260676b2c1fb3
thinkholic <mailtokasun@gmail.com>
2021-10-05 13:44:33 +0530

    Extend console.log

--- src/@core/utils/logger.js
+++ src/@core/utils/logger.js
@@ -1,2 +1,39 @@
-const logger = console;
+/* eslint-disable prefer-spread, no-console, prefer-rest-params */
+import dayjs from 'dayjs';
+import chalk from 'chalk';
+
+const info = chalk.green;
+const warn = chalk.yellow;
+const error = chalk.red;
+
+const logger = {
+  ...console,
+};
+
+const logPrefix = () => {
+  const timestamp = dayjs().format('YYYY-MM-DD/HH:mm:ss');
+  return `[${timestamp}]`;
+};
+
+logger.log = function () {
+  return console.log.apply(
+    console,
+    [logPrefix(), ...arguments].map((arg) => info(arg))
+  );
+};
+
+logger.warn = function () {
+  return console.warn.apply(
+    console,
+    [logPrefix(), ...arguments].map((arg) => warn(arg))
+  );
+};
+
+logger.error = function () {
+  return console.warn.error(
+    console,
+    [logPrefix(), ...arguments].map((arg) => error(arg))
+  );
+};
+
 export default logger;
